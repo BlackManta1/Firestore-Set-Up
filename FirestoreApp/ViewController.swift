7ab2f023a06cc7cd3e5b4ed400ead914bc9d49bc
Saliou DJALO <djalosaliou@gmail.com>
2019-09-10 19:41:03 +0400

    Initial Commit

---
+++ FirestoreApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirestoreApp
+//
+//  Created by Saliou DJALO on 10/09/2019.
+//  Copyright © 2019 Saliou DJALO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
